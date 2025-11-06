24d8595ecf3c74330d7c6eeb5882ede8cb7c622b
Michael Webb <mjwebb1993@yahoo.com>
2025-11-05 21:40:16 -0600

    Added State, not navigo

--- components/main.js
+++ components/main.js
@@ -1,8 +1,7 @@
 import html from "html-literal";
+import * as views from "../views";
 
-export default () => html`
-  <section id="jumbotron">
-    <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
-    <a href="index.html">"Call to Action" "Button"</a>
-  </section>
-`;
+export default state =>
+  html`
+    ${views[state.view](state)}
+  `;
--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+import { header, nav, main, footer } from "./components";
+import * as store from "./store";
+
+function render(state = store.home) {
+  document.querySelector("#root").innerHTML = `
+      ${header(state)}
+      ${nav(store.nav)}
+      ${main(state)}
+      ${footer()}
+    `;
+}
+
+render();
+
 // add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+// document.querySelector(".fa-bars").addEventListener("click", () => {
+//   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+// });
