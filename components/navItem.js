6b3810d427f8cf4207125124b3836fa5d8056656
theokyle <theokyle@gmail.com>
2025-11-06 19:54:43 -0600

    added routing with navigo

--- components/navItem.js
+++ components/navItem.js
@@ -2,6 +2,6 @@ import html from "html-literal";
 
 export default item => {
   return html`
-    <li><a href="${item.url}" title="${item.text}">${item.text}</a></li>
+    <li><a href="${item.url}" title="${item.text}" data-navigo>${item.text}</a></li>
   `;
 };
